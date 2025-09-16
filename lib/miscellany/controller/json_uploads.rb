8ed9a9d4aba759ad43b5c900bb2dd412cb20b973
Ethan Knapp <eknapp@instructure.com>
2025-09-16 15:08:02 -0600

    deepen JsonUploads integration with Rails

--- lib/miscellany/controller/json_uploads.rb
+++ lib/miscellany/controller/json_uploads.rb
@@ -4,32 +4,34 @@ module Miscellany
 
     # This hackery allows using JSON params along with file-uploads
     # On the frontend, use MultiPart form data, and add _parameters as an entry
-    def params
-      if @_params.nil?
-        params_obj = @_params = super
-        phash = params_obj.instance_variable_get(:@parameters)
-        if phash[:_parameters].present?
-          json_layer = JSON.parse(phash.delete(:_parameters))
-          shared_keys = phash.keys & json_layer.keys
-          main_layer = phash.slice(*shared_keys)
-          merge_params(json_layer, main_layer)
-          phash.merge!(json_layer)
-        end
+    def process_action(*)
+      json_param = request.request_parameters[:_parameters]
+      if json_param.present?
+        parsed = JSON.parse(json_param)
+        overlapping_params = request.request_parameters.slice(*(request.request_parameters.keys & parsed.keys))
+        _merge_json_params(parsed, overlapping_params)
+
+        request.parameters.merge! parsed
+        request.request_parameters.merge! parsed
       end
-      @_params
+      super
     end
 
     private
 
-    def merge_params(base, layer)
+    def _wrapper_enabled?
+      (_wrapper_options.format.include?(:json) && request.request_parameters[:_parameters].present?) || super
+    end
+
+    def _merge_json_params(base, layer)
       if base.is_a?(Array) && (layer.is_a?(Hash) || layer.is_a?(Array))
         base.each_with_index.map do |v, i|
           over_key = layer.is_a?(Hash) ? i.to_s : i
-          merge_params(v, layer[over_key])
+          _merge_json_params(v, layer[over_key])
         end
       elsif base.is_a?(Hash) && layer.is_a?(Hash)
         base.each do |k, v|
-          base[k] = merge_params(v, layer[k])
+          base[k] = _merge_json_params(v, layer[k])
         end
         layer.merge(base)
       else
